02b152b1edb7199cdb27177622bd7e57505df23a
chickletboost <chickletboostnigeria@gmail.com>
2026-04-26 23:16:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -233,6 +233,3 @@ bot.launch().then(() => {
 // Graceful stop
 process.once("SIGINT",  () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
-bot.on("message", (ctx) => {
-  console.log("MESSAGE RECEIVED:", ctx.message.text);
-});
